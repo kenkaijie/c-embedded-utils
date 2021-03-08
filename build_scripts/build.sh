68519297874385e7078477aa91182b11576c2cba
Kenneth Kaijie Ng <ken.kaijie@gmail.com>
2021-03-08 13:22:00 +1100

    Fix some naming issues

--- build_scripts/build.sh
+++ build_scripts/build.sh
@@ -4,12 +4,12 @@ docker build -t c-embedded-utils -f ${PROJECT_ROOT}/Dockerfile ${PROJECT_ROOT}
 
 # Build
 
-docker run --rm --mount type=bind,source="${PROJECT_ROOT}",target="/usr/project/" c-embedded-utils "mkdir -p /usr/project/build-temp && cd /usr/project/build-temp && cmake .. && cmake --build ."
+docker run --rm --mount type=bind,source="${PROJECT_ROOT}",target="/usr/project/" c-embedded-utils "mkdir -p /usr/project/build_temp && cd /usr/project/build_temp && cmake .. && cmake --build ."
 
 # Test
 
-docker run --rm --mount type=bind,source="${PROJECT_ROOT}",target="/usr/project/" c-embedded-utils "cd /usr/project/build-temp && ctest -T Test --no-compress-output"
+docker run --rm --mount type=bind,source="${PROJECT_ROOT}",target="/usr/project/" c-embedded-utils "cd /usr/project/build_temp && ctest -T Test --no-compress-output"
 
 # Report
 
-docker run --rm --mount type=bind,source="${PROJECT_ROOT}",target="/usr/project/" c-embedded-utils "cd /usr/project/build-temp && gcovr -r .. . --xml-pretty  --exclude-directories='.*\/extern\/.*' > coverage_report.xml"
+docker run --rm --mount type=bind,source="${PROJECT_ROOT}",target="/usr/project/" c-embedded-utils "cd /usr/project/build_temp && gcovr -r .. . --xml-pretty  --exclude-directories='.*\/extern\/.*' > coverage_report.xml"
