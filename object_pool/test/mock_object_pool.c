01af9024ffbeb94c043c34289a5953dfffa84d1e
Kenneth Kaijie Ng <ken.kaijie@gmail.com>
2021-03-14 22:36:00 +1100

    update mock interface to provide new function

--- object_pool/test/mock_object_pool.c
+++ object_pool/test/mock_object_pool.c
@@ -6,6 +6,7 @@ void mock_object_pool_get_interface(object_pool_t * interface, void * context)
     interface->allocate = (object_pool_allocate_t)mock_object_pool_allocate;
     interface->fetch = (object_pool_fetch_t)mock_object_pool_fetch;
     interface->deallocate = (object_pool_deallocate_t)mock_object_pool_deallocate;
+    interface->get_unused_count = (object_pool_get_unused_count_t)mock_object_pool_get_unused_count;
 }
 
 error_t mock_object_pool_allocate(void * pool, size_t * token)
@@ -33,5 +34,14 @@ error_t mock_object_pool_deallocate(void * pool, size_t * token)
     check_expected_ptr(pool);
     check_expected_ptr(token);
     *token = mock_type(size_t);
+    return mock_type(error_t);
+}
+
+error_t mock_object_pool_get_unused_count(void * pool, size_t * unused_count)
+{
+    function_called();
+    check_expected_ptr(pool);
+    check_expected_ptr(unused_count);
+    *unused_count = mock_type(size_t);
     return mock_type(error_t);
 }
--- object_pool/test/mock_object_pool.h
+++ object_pool/test/mock_object_pool.h
@@ -20,6 +20,7 @@ void mock_object_pool_get_interface(object_pool_t * interface, void * context);
 error_t mock_object_pool_allocate(void * pool, size_t * token);
 error_t mock_object_pool_fetch(void * pool, size_t token, void ** object_pointer);
 error_t mock_object_pool_deallocate(void * pool, size_t * token);
+error_t mock_object_pool_get_unused_count(void * pool, size_t * unused_count);
 
 #define _setup_mock_object_pool_allocate_with_count(with_pool_arg, with_token_arg, returns_val, sets_token_arg, num_times) \
 do {\
@@ -55,3 +56,13 @@ do {\
     } \
 } while(false)
 
+#define _setup_mock_object_pool_get_unused_count_with_count(with_pool_arg, with_unused_count_arg, returns_val, sets_unused_count_arg, num_times) \
+do {\
+    expect_value_count(mock_object_pool_get_unused_count, pool, (uintptr_t)with_pool_arg, num_times);\
+    expect_value_count(mock_object_pool_get_unused_count, unused_count, (uintptr_t)with_unused_count_arg, num_times);\
+    for (size_t idx = 0; idx < num_times; ++idx) \
+    { \
+        will_return(mock_object_pool_get_unused_count, sets_unused_count_arg); \
+        will_return(mock_object_pool_get_unused_count, returns_val); \
+    } \
+} while(false)
