9019599404d8dc73a2b99b50b7390bd8005e2639
Kenneth Kaijie Ng <ken.kaijie@gmail.com>
2021-05-23 22:20:44 +1000

    edit tests abit

--- collections/test/test_circular_buffer.c
+++ collections/test/test_circular_buffer.c
@@ -10,6 +10,7 @@
 #include <string.h>
 
 #define ITEMS_IN_TEST_BUFFER (15)
+#define ITEMS_IN_RIPPLE_INSERT (16)
 
 static void test_interface_nulls(void ** state)
 {
@@ -21,6 +22,35 @@ static void test_interface_nulls(void ** state)
 
     result = circular_buffer_pop_byte(&circular, NULL);
     assert_int_equal(ERR_NULL_POINTER, result);
+}
+
+static void test_ripple_insert(void ** state)
+{
+    static uint8_t buffer[ITEMS_IN_RIPPLE_INSERT];
+    circular_buffer_config_t config = {
+        .buffer = buffer,
+        .buffer_size = ITEMS_IN_RIPPLE_INSERT,
+    };
+    circular_buffer_t circular;
+    error_t result;
+
+    result = circular_buffer_init(&circular, &config);
+    assert_int_equal(ERR_NONE, result);
+
+    // we push 1, and pop 2 (second should always fail)
+    for (uint8_t cycle = 0; cycle < 30; ++cycle)
+    {
+        result = circular_buffer_push_byte(&circular, cycle);
+        assert_int_equal(ERR_NONE, result);
+
+        uint8_t byte_out;
+        result = circular_buffer_pop_byte(&circular, &byte_out);
+        assert_int_equal(ERR_NONE, result);
+        assert_int_equal(byte_out, cycle);
+
+        result = circular_buffer_pop_byte(&circular, &byte_out);
+        assert_int_equal(ERR_EMPTY, result);
+    }
 }
 
 static void test_correct_ordering_with_overflow(void ** state)
@@ -45,43 +75,43 @@ static void test_correct_ordering_with_overflow(void ** state)
         assert_int_equal(idx + 1, buffer_size);
     }
 
-    // buffer should have 0...14
+    for (uint8_t idx = ITEMS_IN_TEST_BUFFER; idx < (ITEMS_IN_TEST_BUFFER * 2); ++idx)
+    {
+        result = circular_buffer_push_byte(&circular, idx);
+        assert_int_equal(ERR_OVERRIDE, result);
 
-    uint8_t test_byte = ITEMS_IN_TEST_BUFFER;
-    result = circular_buffer_push_byte(&circular, test_byte);
-    assert_int_equal(ERR_OVERRIDE, result);
+        size_t buffer_size = circular_buffer_get_size(&circular);
+        assert_int_equal(ITEMS_IN_TEST_BUFFER, buffer_size);
+    }
 
-    result = circular_buffer_push_byte(&circular, test_byte + 1);
-    assert_int_equal(ERR_OVERRIDE, result);
+    // buffer should have 15 ... 29
 
-    // override 0 with 15, so the buffer is now 2...16
-
-    // pop out 5 items, should be 2, 3, 4, 5, 6
+    // pop out 5 items, should be 15, 16, 17, 18, 19
     for (uint8_t idx = 0; idx < 5; ++idx)
     {
         uint8_t byte_out;
         result = circular_buffer_pop_byte(&circular, &byte_out);
         assert_int_equal(ERR_NONE, result);
-        assert_int_equal(byte_out, idx + 2);
+        assert_int_equal(byte_out, idx + 15);
     }
 
-    // push in 3 items 17.. 19
-    for (uint8_t idx = 0; idx < 3; ++idx)
+    // push in 3 items 30 .. 32
+    for (uint8_t idx = 30; idx < 33; ++idx)
     {
-        result = circular_buffer_push_byte(&circular, test_byte + 2 + idx);
+        result = circular_buffer_push_byte(&circular, idx);
         assert_int_equal(ERR_NONE, result);
     }
 
-    // pop out the rest, 7 ... 19
+    // pop out the rest, 20 ... 32
     size_t items_in_buffer = circular_buffer_get_size(&circular);
     assert_int_equal(13, items_in_buffer);
 
-    for (uint8_t idx = 0; idx < 13; ++idx)
+    for (uint8_t idx = 20; idx < (20 + 13); ++idx)
     {
         uint8_t byte_out;
         result = circular_buffer_pop_byte(&circular, &byte_out);
         assert_int_equal(ERR_NONE, result);
-        assert_int_equal(byte_out, idx + 7);
+        assert_int_equal(byte_out, idx);
     }
 
     uint8_t byte_out;
@@ -141,6 +171,7 @@ int test_circular_buffer_run_tests(void)
 {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_interface_nulls),
+        cmocka_unit_test(test_ripple_insert),
         cmocka_unit_test(test_correct_ordering_with_overflow),
         cmocka_unit_test(test_bad_config),
         cmocka_unit_test(test_deinit_prevents_actions),
